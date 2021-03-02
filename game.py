e4ec64ef1741fc97af691a65f3e841363fefbfcd
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-02 16:32:27 +0000

    set the basics and velocities

--- game.py
+++ game.py
@@ -3,3 +3,9 @@ import random
 import pygame
 from pygame.locals import *
 
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.velocity = 5
+        self. max_velocity = 5
